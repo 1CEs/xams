9bc25266e8e75cb32ea36f65f43ec8d56b79934b
1CEs <isongwut.me@gmail.com>
2024-11-01 14:30:25 +0700

    update: get category controller

--- backend/src/presentation/controllers/category,controller.ts
+++ backend/src/presentation/controllers/category,controller.ts
@@ -6,8 +6,13 @@ import { InstructorService } from "../../core/user/service/instructor.service";
 import { UserServiceFactory } from "../../core/user/service/user.service";
 import { UserType } from "../../types/user";
 import { errorResponse } from "../../utils/error";
+import { IInstructor } from "../../models/interface/user/instructor";
 
 export const CategoryController = new Elysia({ prefix: 'category' })
+    .use(verifyToken)
+    .get('', ({ user }) => {
+        return (user as IInstructor).my_category
+    })
     .use(verifyToken)
     .patch('', ({ body }) => body, {
         body: CategoryBodySchema,
