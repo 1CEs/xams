393817936b69e05a7078f1af3fda6625566badea
Palk1996 <pimmsam82@gmail.com>
2024-12-06 18:17:28 +0700

    chore: completed course model

---
+++ backend/src/core/course/model/interface/setting.ts
@@ -0,0 +1,12 @@
+export interface ISetting {
+    exam_id: string
+    open_time: Date
+    close_time: Date
+    ip_range: string
+    exam_code: string
+    allowed_attempts: number
+    allowed_review: boolean
+    show_answer: boolean
+    randomize_question: boolean
+    randomize_choice: boolean
+}
